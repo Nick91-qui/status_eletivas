6c8d28ee2fc767a248305df7b7b4c69fba090e63
NICHOLAS CONTIJO MOREIRA <68407414+Nick91-qui@users.noreply.github.com>
2025-03-04 09:34:05 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,44 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.3.1/firebase-app.js";
+import { getFirestore, collection, getDocs } from "https://www.gstatic.com/firebasejs/11.3.1/firebase-firestore.js";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAqZBVNO_jIjah9v-Tp_Axy1LoMLkaINPU",
+    authDomain: "device-streaming-9e3b934a.firebaseapp.com",
+    projectId: "device-streaming-9e3b934a",
+    storageBucket: "device-streaming-9e3b934a.appspot.com",
+    messagingSenderId: "608328398854",
+    appId: "1:608328398854:web:706cf69b6dcb751930ab87"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+async function carregarInscricoes() {
+    const tbody = document.getElementById("inscricoes-list");
+    tbody.innerHTML = "";
+    const alunosSnapshot = await getDocs(collection(db, "alunos"));
+    const inscricoes = [];
+
+    alunosSnapshot.forEach(doc => {
+        const aluno = doc.data();
+        if (aluno.inscrito) {
+            inscricoes.push({
+                eletiva: aluno.eletiva,
+                turma: aluno.turma,
+                nomeAluno: aluno.nomeAluno
+            });
+        }
+    });
+
+    inscricoes.sort((a, b) => a.eletiva.localeCompare(b.eletiva));
+
+    inscricoes.forEach(inscricao => {
+        tbody.innerHTML += `<tr>
+            <td>${inscricao.eletiva}</td>
+            <td>${inscricao.turma}</td>
+            <td>${inscricao.nomeAluno}</td>
+        </tr>`;
+    });
+}
+
+carregarInscricoes();
